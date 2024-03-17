2c42eb9ea041fc62250b9a0dc3d7b932f11f6dd5
Yermek Kapushev <kapushev@gmail.com>
2024-03-17 15:53:19 +0300

    Added experiments/create_index.sh

---
+++ experiments/create_index.sh
@@ -0,0 +1,10 @@
+# resulting index dir will be index_root/experiment_name/index_name
+python ./scripts/colbert_index.py \
+  --checkpoint_path ./checkpoints/colbertv2.0/ \
+  --collection_path ./data/leandojo_benchmark_4/random/colbert_collection.tsv \
+  --index_root ./experiments/ \
+  --experiment_name lean \
+  --index_name colbert_v1 \
+  --index_bsize 64 \
+  --overwrite true
+
